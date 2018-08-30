36501ae72de54d1707bf687963d5666baae7b8a0
meilier <1967562123@qq.com>
2018-08-30 23:36:51 +0800

    unset annotation

--- main.go
+++ main.go
@@ -15,23 +15,24 @@ var fabricSetups map[string]*blockchain.FabricSetup
 func main() {
 	fabricSetups = make(map[string]*blockchain.FabricSetup)
 	// Definition of the Fabric SDK properties
-	for k, am := range webutil.Orgnization {
-		if k == "sales" || k == "smartphone" {
-			for _, user := range am {
-				fabricSetups[user.UserName] = &blockchain.FabricSetup{
-					User:       user.UserName,
-					Secret:     user.Secret,
-					ConfigFile: user.FilePath,
-				}
-				// Initialization of the Fabric SDK from the previously set properties
-				err := fabricSetups[user.UserName].Initialize()
-				if err != nil {
-					fmt.Printf("Unable to initialize the sFabric SDK: %v\n", err)
-					return
-				}
+	//for k, am := range webutil.Orgnization {
+	for _, am := range webutil.Orgnization {
+		//if k == "sales" || k == "smartphone" {
+		for _, user := range am {
+			fabricSetups[user.UserName] = &blockchain.FabricSetup{
+				User:       user.UserName,
+				Secret:     user.Secret,
+				ConfigFile: user.FilePath,
 			}
-
+			// Initialization of the Fabric SDK from the previously set properties
+			err := fabricSetups[user.UserName].Initialize()
+			if err != nil {
+				fmt.Printf("Unable to initialize the sFabric SDK: %v\n", err)
+				return
+			}
 		}
+
+		//}
 	}
 
 	// Close SDK
